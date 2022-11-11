208d50fdcd707c5e7191a521d851cea368a2e8cd
omishyn <omishyn@datamola.com>
2022-11-11 18:56:38 +0200

    fix version 14.0.1

--- projects/app/src/polyfills.ts
+++ projects/app/src/polyfills.ts
@@ -1,1 +1,4 @@
-import 'zone.js/dist/zone';
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js';
