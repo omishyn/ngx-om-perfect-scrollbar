bf715fc912b2b83b6bb309ccc5ec82ede1f42ecc
omishyn <omishyn@datamola.com>
2023-04-25 21:47:03 +0300

    add fixModules

---
+++ fix-modules.js
@@ -0,0 +1,24 @@
+const fse = require("fs-extra");
+const path = require("path");
+
+function getSource(name) {
+    return path.join(__dirname, `./projects/fix/${name}`);
+}
+
+function getDest(name) {
+    return path.join(__dirname, `./node_modules/gui/build/${name}`);
+}
+
+function fixModules() {
+    try {
+        ['resize-observer-polyfill'].forEach(async (name) => {
+            await fse.copy(getSource(name), getDest(name));
+        });
+
+        console.log("files in assets has been copy to dist");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+fixModules();
